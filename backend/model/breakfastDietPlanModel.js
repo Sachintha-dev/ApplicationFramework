24509b23c162272b8e9115bc11c26e6781586225
Senula Nanayakkara <senulananayakkara88@gmail.com>
2023-05-01 14:23:24 +0530

    Cretate diet plan model breakfast

---
+++ backend/model/breakfastDietPlanModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+//Creating common model for diet plan storing
+const breakfastDietPlanScheme = new mongoose.Schema({
+  playerID: {
+    type: String,
+    required: true,
+  },
+
+  playerName: {
+    type: String,
+    required: true,
+  },
+
+  dietPlan: [
+    {
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+
+      calories: {
+        type: Number,
+        required: true,
+      },
+
+      dietDescription: {
+        type: String,
+        required: true,
+      },
+      dietID: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const DietPlan = mongoose.model("DietPlan", breakfastDietPlanScheme);
+module.exports = DietPlan;
