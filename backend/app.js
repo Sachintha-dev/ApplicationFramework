de13698d2df8f287c26d5a628a8ddaeb67ef04a2
Senula Nanayakkara <senulananayakkara88@gmail.com>
2023-05-01 14:33:13 +0530

    Complete backend for dietient

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
+
+const brekfastDiet = require("./routes/breakfastDietPlanRoute");
 app.use(express.json());
 
 const PORT = 5012;
+
+app.use("/api/breakfastPlan/", brekfastDiet);
 
 mongoose.connect(
   "mongodb+srv://senulananayakkara88:gwPB7uWsD8eKIYfO@cricketdb.ozfnpql.mongodb.net/test",
