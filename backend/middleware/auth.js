7eee6482665ca002da2233d779b95991cfc861d1
sachintha <sudehsachintha00@gmail.com>
2023-05-07 21:39:37 +0530

    done:crickter middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -19,4 +19,51 @@ function localVariable(req, res, next) {
   next();
 }
 
+const adminMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Authorization header missing" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, key);
+    req.user = decoded.user;
+    console.log("user access done");
+    if (req.user.userrole !== "Admin" || req.user.userrole !== "admin") {
+      return res.status(403).send({ message: "Admin Access denied" });
+    }
+  } catch {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+  next();
+};
+const cricketerMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Authorization header missing" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, key);
+    req.user = decoded.user;
+    // Check user role
+    if (toLowerCase(req.user.userrole) !== "cricketer") {
+      return res.status(403).send({ message: "Cricketer Access denied" });
+    }
+  } catch {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+  next();
+};
+
 module.exports = { Auth, localVariable };
